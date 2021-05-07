b85b9102dcf9de68bd838ccc5f1a4fff203a6275
Alek Hrycaiko <alekhrycaiko@gmail.com>
2021-05-07 07:43:04 -0700

    uncomment and ensure test is using tokio

--- tests/app_codegen/mod.rs
+++ tests/app_codegen/mod.rs
@@ -39,8 +39,8 @@ async fn test_with_options_and_trailing_comma() {
         task_time_limit = 2,
     );
 }
-/*
-#[test]
+
+#[tokio::test]
 fn test_with_empty_queues_keywords() {
     let _app = celery::app!(
         broker = AMQP { std::env::var("AMQP_ADDR").unwrap() },
@@ -49,4 +49,3 @@ fn test_with_empty_queues_keywords() {
         queues=vec![]
     );
 }
-*/
